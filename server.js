893e31c8c9c90d893aa697b7e3823b7790fc9293
bobbyschank <robertschank@gmail.com>
2017-04-10 18:38:55 -0600

    index show create update

--- server.js
+++ server.js
@@ -65,19 +65,87 @@ app.get('/api', function api_index(req, res) {
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Bob Schank 101"},
 
-      {method: "GET", path: "/api/food", description: "View all food Bob Schank can cook"},
-      {method: "GET", path: "/api/food/:id", description: "Find a single food by id"},
-      {method: "POST", path: "/api/food", description: "Add a dish to my repertoire."}
+      {method: "GET", path: "/api/foods", description: "View all the food Bob Schank can cook"},
+      {method: "GET", path: "/api/foods/:id", description: "Find a single food by id"},
+      {method: "POST", path: "/api/foods", description: "Add a dish to my repertoire."}
     ]
   });
 });
 
 // 
 
-app.get('/api/profile', function api_index(req, res) {
+app.get('/api/profile', function (req, res) {
   // TODO: Document all your api endpoints below
   res.json(profile);
 });
+
+
+// INDEX
+app.get('/api/foods', function (req, res) {
+  db.Food.find({}, function(err, foods) {
+    if (err) { console.log('error: ', err);}
+    res.json(foods);
+  });
+});
+
+// SHOW
+// (Not sure why this one won't work.)
+// app.get('api/foods/:_id', function (req, res) {
+//   console.log('req.params._id', req.params._id);
+//   db.Food.findOne({_id: req.params._id }, function(err, food) {
+//     res.json(food);
+//   });
+// });
+
+// SHOW
+app.get('/api/foods/:_id', function (req, res) {
+  console.log('req.params._id', req.params._id);
+  db.Food.findOne({_id: req.params._id }, function(err, food) {
+    res.json(food);
+  });
+});
+
+// CREATE
+app.post('/api/foods', function (req, res) {
+  var newFood = new db.Food({
+    name: req.body.name,
+    notes: req.body.notes,
+    cookTime: req.body.cookTime,
+    proficiency: req.body.proficiency,
+    partyReady: req.body.partyReady
+  });
+  newFood.save(function(err, food) {
+    if (err) {console.log('save error: ', err);}
+    console.log('saved', food.name);
+    res.json(food);
+  });
+});
+
+// UPDATE
+app.post('/api/foods/:_id', function (req, res) {
+  console.log('req.params._id', req.params._id);
+  var newFood = new db.Food({
+    name: req.body.name,
+    notes: req.body.notes,
+    cookTime: req.body.cookTime,
+    proficiency: req.body.proficiency,
+    partyReady: req.body.partyReady
+  });
+  db.Food.findOne({_id: req.params._id }, function(err, food) {
+    if (err) {console.log('save error: ', err);}
+    food.name = req.body.name;
+    food.notes = req.body.notes;
+    food.cookTime = req.body.cookTime;
+    food.proficiency = req.body.proficiency;
+    food.partyReady = req.body.partyReady;
+    food.save(function(err, food) {
+      if (err) {console.log('save error: ', err);}
+      console.log('saved', food.name);
+      res.json(food);
+    });
+  });
+});
+
 
 /**********
  * SERVER *
